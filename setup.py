4e78778553614ac928026a1b8bb977f8525a748a
Ryan-dayone <115816983+Ryan-dayone@users.noreply.github.com>
2023-02-09 16:43:39 -0800

    intial commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+"""
+
+Author: Ryan Morlando
+Created: 
+Updated: 
+V1.0.0
+Patch Notes:
+
+To Do:
+
+"""
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='criteo-api',
+    version='1.0.0',
+    author='Ryan Morlando',
+    author_email='ryan@dayonedigital.com',
+    description='Criteo API access',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Ryan-dayone/criteo-api',
+    license='MIT',
+    packages=['criteo_api'],
+    install_requires=['requests', 'json', 'os', 'pandas', 'time'],
+)
